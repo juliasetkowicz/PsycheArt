1b64d9ccc6ec8496dbc108874fde63db5dfdd13f
Julia Setkowicz <seciadone@gmail.com>
2020-01-16 16:56:41 +0100

    eventy

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ const welcome = (name, age) => {
     console.log('witaj ${name}, masz ${age} lat!');
 }
 welcome('marta', 23)
+
+const button = document.querySelector('.header__button--js');
+console.log(button)
+
+button.addEventListener('click', (e) => {
+const header = document.querySelector('.header__title--js');
+header.innerHTML = 'klik, klik';
+header.classList.toggle('header__title--red');
+alert('hello');
+});
